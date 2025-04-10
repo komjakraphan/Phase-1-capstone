913b2d776946889c6dca859b616cb5c113209828
Grant komjakraphan <komjakraphan@gmail.com>
2025-04-10 13:00:56 -0700

    created and linked navigation

--- form-capstone/src/app/app.component.ts
+++ form-capstone/src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 import { ClientFormComponent } from './client-form/client-form.component';
 import { MeetingFormComponent } from './meeting-form/meeting-form.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ClientFormComponent, MeetingFormComponent],
+  imports: [RouterOutlet, ClientFormComponent, MeetingFormComponent, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- form-capstone/src/app/app.routes.ts
+++ form-capstone/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { ClientFormComponent } from './client-form/client-form.component';
+import { MeetingFormComponent } from './meeting-form/meeting-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: 'client-form', pathMatch:'full'},
+    {path:'client-form', component: ClientFormComponent},
+    {path: 'meeting-form', component: MeetingFormComponent}
+];
